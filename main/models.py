de919137efbcee04b45124e13979aa015f70999a
mukule <nelsonmasibo6@gmail.com>
2023-06-21 12:04:18 +0300

    changed required fields

--- main/models.py
+++ main/models.py
@@ -7,11 +7,11 @@ class LicenseCategory(models.Model):
         return self.name
     
 class License(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    license_number = models.CharField(max_length=50)
-    weblink = models.URLField()
-    category = models.ForeignKey('LicenseCategory', on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    license_number = models.CharField(max_length=50, blank=True)
+    weblink = models.URLField(blank=True)
+    category = models.ForeignKey('LicenseCategory', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
